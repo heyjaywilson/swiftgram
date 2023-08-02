ad5ee2616c5b71c5cfa4f9d09e1f32884db3e07c
Jay <heyjay@cctplus.dev>
2023-08-01 20:32:08 -0500

    feat: barebones tab bar

---
+++ SwiftGramKit/Sources/AppCore/AppCoreView.swift
@@ -0,0 +1,39 @@
+//
+//  AppCoreView.swift
+//  SwiftGram
+//
+//  Created by Jay on 8/1/23.
+//
+
+import SwiftUI
+
+enum AppTabType {
+    case home, search, new, video, profile
+}
+
+struct AppCoreView: View {
+    @State private var selectedTab = AppTabType.home
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Home")
+                .tabItem { Image(systemName: "house") }
+                .tag(AppTabType.home)
+            Text("Search")
+                .tabItem { Image(systemName: "magnifyingglass") }
+                .tag(AppTabType.home)
+            Text("New")
+                .tabItem { Image(systemName: "plus.square") }
+                .tag(AppTabType.home)
+            Text("Video")
+                .tabItem { Image(systemName: "movieclapper") }
+                .tag(AppTabType.home)
+            Text("Profile")
+                .tabItem { Image(systemName: "person.circle") }
+                .tag(AppTabType.home)
+        }
+    }
+}
+
+#Preview {
+    AppCoreView()
+}
